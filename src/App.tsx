288be4e227ed2f5195500cd82ce315ba7ab3bc69
Noudea <oudea.nicolas@gmail.com>
2023-04-12 11:20:36 +0200

    :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView, Text
+} from "react-native";
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>Salut</Text>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+export default App;
